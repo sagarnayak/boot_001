0d32024c92c12e3b06e3121f3ab7deebc52d3f23
Stark <sagar@agpaytech.co.uk>
2021-08-26 16:55:53 +0530

    update

--- src/main/kotlin/com/example/boot_001/entities/User.kt
+++ src/main/kotlin/com/example/boot_001/entities/User.kt
@@ -1,13 +1,12 @@
 package com.example.boot_001.entities
 
 import java.util.*
-import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.validation.constraints.NotBlank
 
 @Entity(name = "users")
 data class User(
-    @Id @Column(columnDefinition = " uuid default gen_random_uuid()") val id: UUID = UUID.randomUUID(),
+    @Id val id: UUID = UUID.randomUUID(),
     @field:NotBlank val name: String = ""
 )
