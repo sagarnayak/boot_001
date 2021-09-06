70557e3cf8f8ff4683dd8cb8160070956fb0eb3e
Stark <sagar@agpaytech.co.uk>
2021-09-06 18:02:13 +0530

    working on spring security

--- src/main/kotlin/com/example/boot_001/Boot001Application.kt
+++ src/main/kotlin/com/example/boot_001/Boot001Application.kt
@@ -1,11 +1,16 @@
 package com.example.boot_001
 
+import com.example.boot_001.entities.JWTConfig
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(
+        JWTConfig::class
+)
 class Boot001Application
 
 val log: Logger = LoggerFactory.getLogger(Boot001Application::class.java)
---
+++ src/main/kotlin/com/example/boot_001/entities/UsernameAndPassword.kt
@@ -0,0 +1,6 @@
+package com.example.boot_001.entities
+
+data class UsernameAndPassword(
+        var userName: String = "",
+        var password: String = ""
+)
